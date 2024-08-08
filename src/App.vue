910998798c6f6c013c508651d0f68cd7d8469327
Villeman <villeman.arm@gmail.com>
2024-08-08 08:08:12 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import NewComponent from './components/NewComponent.vue'
+</script>
+
+<template>
+  <NewComponent msg="You did it!" />
+
+</template>
+
+<style scoped lang="sass">
+  @import '@/assets/styles/constants.sass'
+
+</style>
